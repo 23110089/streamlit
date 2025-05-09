0445dd8fce4e5a4d3292206a8bb015d1cc80eea4
23110089 <152385695+23110089@users.noreply.github.com>
2025-05-09 16:46:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.title("Nhúng web trong Streamlit")
-components.iframe("https://www.google.com", height=600)
+components.iframe("https://idx.google.com/", height=600)
