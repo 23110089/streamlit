37e14b3afa948cd8e367eca876731f315e63f6d0
23110089 <152385695+23110089@users.noreply.github.com>
2025-05-09 16:55:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,13 @@ from selenium.webdriver.chrome.options import Options
 import streamlit as st
 import time
 
-# Nhập URL từ người dùng
-url = st.text_input("Nhập URL của website muốn truy cập", "https://example.com")
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
 
-# Tạo nút để chạy Selenium
-if st.button("Truy cập web"):
-    options = Options()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
-    driver.get("https://idx.google.com/"); time.sleep(3)
-    title = driver.title
-    st.write(f"Tiêu đề của trang web: {title}")
-    driver.quit()
+driver = webdriver.Chrome(options=options)
+driver.get("https://idx.google.com/"); time.sleep(3)
+title = driver.title
+st.write(f"Tiêu đề của trang web: {title}")
+driver.quit()
