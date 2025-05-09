6a4213cbb1acfaf2625feecbbf7ad33096c77d91
23110089 <152385695+23110089@users.noreply.github.com>
2025-05-09 17:24:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,21 +3,3 @@ from time import sleep
 from streamlit import empty; out = empty()
 
 out.empty(); out.write('há há')
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument('--headless')
-
-# Khởi động trình duyệt
-driver = webdriver.Chrome(options=options)
-
-# Mở trang web
-driver.get("https://www.google.com")
-
-# Tìm ô tìm kiếm và nhập từ khóa
-search_box = driver.find_element(By.NAME, "q")
-search_box.send_keys("Hello Selenium")
-search_box.submit()
