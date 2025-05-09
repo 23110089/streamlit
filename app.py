bc593b3728e74d946d12625e54dccc6bbc9cd872
23110089 <152385695+23110089@users.noreply.github.com>
2025-05-09 17:20:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ out.empty(); out.write('há há')
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 # Khởi động trình duyệt
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 # Mở trang web
 driver.get("https://www.google.com")
