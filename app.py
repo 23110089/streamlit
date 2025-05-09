bef635703924598d25b4e2062995070cf4814c2f
23110089 <152385695+23110089@users.noreply.github.com>
2025-05-09 17:12:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,4 @@ from time import sleep
 import streamlit as st
 
 out = st.empty()
-while True:
-  sleep(10)
-  out.empty()
-  out.write(get('https://23110089.streamlit.app/').text)
+out.empty(); out.write('haha')
